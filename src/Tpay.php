64ab25860e7d799aff20fdaf3fcf6df98cee4315
Oleksandr Borodaichuk <alexandr.borodaichuk@gmail.com>
2023-06-19 07:49:38 +0200

    Added a module files

---
+++ src/Tpay.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace stitchua\tpay;
+
+/**
+ * tpay module definition class
+ */
+class Tpay extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\tpay\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
